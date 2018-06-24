812729df875b7f1f9189579fb54121e549bb7a52
Leo Schwarz <mail@leoschwarz.com>
2018-06-24 15:56:35 +0200

    MEDIT: Label support.

--- meshio/medit_io.py
+++ meshio/medit_io.py
@@ -19,9 +19,9 @@ from .mesh import Mesh
 
 def read(filename):
     with open(filename) as f:
-        points, cells = read_buffer(f)
+        points, cells, point_data, cell_data = read_buffer(f)
 
-    return Mesh(points, cells)
+    return Mesh(points, cells, point_data, cell_data)
 
 
 class _ItemReader:
@@ -59,8 +59,32 @@ class _ItemReader:
 def read_buffer(file):
     dim = 0
     cells = {}
+    point_data = {}
+    cell_data = {}
 
     reader = _ItemReader(file)
+
+    meshio_from_medit = {
+        "Edges": ("line", 2),
+        "Triangles": ("triangle", 3),
+        "Quadrilaterals": ("quad", 4),
+        "Tetrahedra": ("tetra", 4),
+        "Hexahedra": ("hexahedra", 8),
+    }
+
+    # key = keyword, value = number of values per entry
+    ignored_fields = {
+        "Corners": 1,
+        "RequiredVertices": 1,
+        "Ridges": 1,
+        "RequiredEdges": 1,
+        "Normals": 3,
+        "Tangents": 3,
+        "NormalAtVertices": 2,
+        "NormalAtTriangleVertices": 3,
+        "NormalAtQuadrilateralVertices": 3,
+        "TangentAtEdges": 3,
+    }
 
     while True:
         try:
@@ -87,25 +111,35 @@ def read_buffer(file):
             # The first value is the number of nodes
             num_verts = int(reader.next_item())
             points = numpy.empty((num_verts, dim), dtype=float)
+            point_data[0] = numpy.empty(num_verts, dtype=int)
             for k in range(num_verts):
-                # Throw away the label immediately
-                points[k] = numpy.array(reader.next_items(dim + 1), dtype=float)[:-1]
+                # Read point and point data.
+                data = numpy.array(reader.next_items(dim + 1), dtype=float)
+                points[k] = data[:-1]
+                point_data[0][k] = data[-1]
         elif keyword in meshio_from_medit:
             meshio_name, num = meshio_from_medit[keyword]
             # The first value is the number of elements
             num_cells = int(reader.next_item())
-            cell_data = numpy.empty((num_cells, num), dtype=int)
+            cells[meshio_name] = numpy.empty((num_cells, num), dtype=int)
+            cell_data[meshio_name] = {0: numpy.empty(num_cells, dtype=int)}
             for k in range(num_cells):
                 data = numpy.array(reader.next_items(num + 1), dtype=int)
-                # Throw away the label
-                cell_data[k] = data[:-1]
+                # Store cell values and cell labels.
+                cells[meshio_name][k] = data[:-1]
+                cell_data[meshio_name][0][k] = data[-1]
 
             # adapt 0-base
-            cells[meshio_name] = cell_data - 1
+            cells[meshio_name] -= 1
+        elif keyword in ignored_fields:
+            print("Warning: Field {} currently ignored by meshio.".format(keyword))
+            # Skip the values.
+            n_values = int(reader.next_item()) * ignored_fields[keyword]
+            reader.next_items(n_values)
         else:
             assert keyword == "End", "Unknown keyword '{}'.".format(keyword)
 
-    return points, cells
+    return points, cells, point_data, cell_data
 
 
 def write(filename, mesh):
@@ -122,7 +156,14 @@ def write(filename, mesh):
         # vertices
         fh.write(b"\nVertices\n")
         fh.write("{}\n".format(n).encode("utf-8"))
+        # vertex labeles
         labels = numpy.ones(n, dtype=int)
+        if mesh.point_data:
+            if 0 in mesh.point_data:
+                labels = mesh.point_data[0]
+            elif mesh.point_data.keys():
+                assert len(mesh.point_data) == 1, "Only 1-D labels supported."
+                labels = list(mesh.point_data.values())[0]
         data = numpy.c_[mesh.points, labels]
         fmt = " ".join(["%r"] * d) + " %d"
         numpy.savetxt(fh, data, fmt)
@@ -144,10 +185,20 @@ def write(filename, mesh):
                 )
                 logging.warning(msg)
                 continue
+
             fh.write(b"\n")
             fh.write("{}\n".format(medit_name).encode("utf-8"))
             fh.write("{}\n".format(len(data)).encode("utf-8"))
+
+            # cell data
             labels = numpy.ones(len(data), dtype=int)
+            if mesh.cell_data:
+                if 0 in mesh.cell_data[key]:
+                    labels = mesh.cell_data[key][0]
+                else:
+                    assert len(mesh.cell_data[key]) == 1, "Only 1-D labels supported."
+                    labels = list(mesh.cell_data[key].values())[0]
+
             # adapt 1-base
             data_with_label = numpy.c_[data + 1, labels]
             fmt = " ".join(["%d"] * (num + 1))
